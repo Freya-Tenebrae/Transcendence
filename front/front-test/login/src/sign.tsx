febc35abb0188189a0dd0c69a7f1fdecafa7e8f6
Pierre-marie LAM <plam@made-f0Cr7s5.clusters.42paris.fr>
2023-09-27 16:58:34 +0200

    added a buton function to test with the others functions

---
+++ front/front-test/login/src/sign.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import './sign.css'
+
+interface Props {
+	border: string;
+	color: string;
+	children?: React.ReactNode;
+	height: string;
+	onClick: () => void;
+	width: string;
+  }
+
+const Button: React.FC<Props> = ({ 
+    border,
+    color,
+    children,
+    height,
+    onClick,
+    width
+  }) => { 
+  return (
+    <button 
+      onClick={onClick}
+      style={{
+         backgroundColor: color,
+         border,
+         height,
+         width
+      }}
+    >
+    {children}
+    </button>
+  );
+}
+
+export default Button;
