c97c91e28fe484a35918c5c2b452c7d4bb430eba
Pierre-marie LAM <plam@made-f0Cr7s5.clusters.42paris.fr>
2023-09-27 16:29:43 +0200

    added profile page for tests and react, later replaced with nuxtjs

--- front/front-test/login/src/App.js
+++ front/front-test/login/src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import React, { useState } from "react";
 import LoginForm from "./LoginForm";
 import Header from "./Header";
+import Sign from './Sign'
 
 export default function App() {
 	const [isShowLogin, setIsShowLogin] = useState(true);
---
+++ front/front-test/login/src/sign.js
@@ -0,0 +1,11 @@
+import React from "react";
+import LoginForm from './LoginForm'
+import './sign.css'
+
+const Sign = ({isShowLogin}) => {
+	<div className='Sign'>
+		return(LoginForm(isShowLogin));
+	</div>
+};
+
+export default sign;
