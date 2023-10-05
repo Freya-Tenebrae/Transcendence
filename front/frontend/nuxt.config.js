6aeb0fd084c59b2ff955fa05c1f6072b2d77e1c2
Pierre-marie LAM <plam@bess-f4r3s10.clusters.42paris.fr>
2023-10-05 17:02:21 +0200

    test front container, issues with the deployment method, maybe instead of SSR, go static ?

--- front/frontend/nuxt.config.js
+++ front/frontend/nuxt.config.js
@@ -1,7 +1,12 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
+
+  // Single Page Application Deployment: https://nuxtjs.ir/guide/commands
   mode: 'spa',
+
+  // Target: https://go.nuxtjs.dev/config-target
+  target: 'static',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
