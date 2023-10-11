97efdbc4a300fb589ee5ad46b0fe9abe5d282bb8
Pierre-marie LAM <plam@bess-f4r2s5.clusters.42paris.fr>
2023-10-11 12:06:39 +0200

    started the game implementation

--- front/frontend/components/Game.vue
+++ front/frontend/components/Game.vue
@@ -15,7 +15,7 @@ var game;
 const PLAYER_HEIGHT = 100;
 const PLAYER_WIDTH = 5;
 
-//Draw the game zone
+//Draw the game zone : https://www.alsacreations.com/tuto/lire/1484-introduction.html
 function draw() {
 	var context = canvas.getContext('2d');
 	
@@ -34,7 +34,7 @@ function draw() {
 	context.fillRect(0, game.player.y, PLAYER_WIDTH, PLAYER_HEIGHT);
 	context.fillRect(canvas.width - PLAYER_WIDTH, game.computer.y, PLAYER_WIDTH, PLAYER_HEIGHT);
 	
-	// Draw ball
+	// Draw ball, maybe replaced later
 	context.beginPath();
 	context.fillStyle = 'white';
 	context.arc(game.ball.x, game.ball.y, game.ball.r, 0, Math.PI * 2, false);
@@ -66,12 +66,30 @@ document.addEventListener('DOMContentLoaded', function () {
 	play();
 });
 
+// Mouse move event
+canvas.addEventListener('mousemove', player1Move);
+
+
+// Ball animation
 function play() {
 	game.ball.x += game.ball.speed.x;
 	game.ball.y += game.ball.speed.y;
 	draw();
+	player2Move();			// will change later
 
 	requestAnimationFrame(play);
+}
+
+// Players movement
+function player1Move(event) {
+	var canvasLocation = canvas.getBoundingClientRect();
+	var mouseLocation = event.clientY - canvasLocation.y;
+
+	game.player1.y = mouseLocation - PLAYER_HEIGHT/2;
+}
+
+function player2Move() {
+	game.player2.y += game.ball.speed.y * 0.85; // will change when backend will be linked for multiplayer tests, for now just a bot playing
 }
 
 </script>
