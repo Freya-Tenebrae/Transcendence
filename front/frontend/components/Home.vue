c77213d3b3ed7d9b69bb3e19402a0d861b9d210c
Pierre-marie LAM <plam@bess-f4r3s8.clusters.42paris.fr>
2023-10-13 11:27:52 +0200

    testing on the front render to match the backend datas

--- front/frontend/components/Home.vue
+++ front/frontend/components/Home.vue
@@ -120,6 +120,8 @@ export default {
     // context.fillRect(canvas.width - PLAYER_WIDTH, game.computer.y, PLAYER_WIDTH, PLAYER_HEIGHT);
 
     // Draw ball
+    // (canvas.width /2, canvas.height / 2) should be defined as the new (0,0)
+    // and canvas.width /2, canvas.height / 2 should be the new unit (+1,0, +1,0) to match the backend unit method
     context.beginPath();
     context.fillStyle = 'white';
     context.arc(canvas.width /2, canvas.height / 2, 5, 0, Math.PI * 2, false);
